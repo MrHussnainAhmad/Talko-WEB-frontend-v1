819130e8ca257b4d7804e3f7bb311124442ab48a
Hussnain Ahmad <mrhussnainahmad@gmail.com>
2025-06-19 22:00:36 +0500

    updated theme default state to 'light'

--- src/store/useThemeStore.js
+++ src/store/useThemeStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-    theme : localStorage.getItem("theme") || "dark",
+    theme : localStorage.getItem("theme") || "light",
     setTheme: (theme) => {
         localStorage.setItem("theme", theme);
         set({ theme });
--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss(), daisyui],
   daisyui: {
     themes: [
-      "light", "dark", "cupcake", "bumblebee", "emerald", "corporate",
-      "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden",
-      "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black",
-      "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade",
-      "night", "coffee", "winter", "dim", "nord", "sunset",
+      "light", "dark",
     ]
   },
   server: {
