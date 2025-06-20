7ca19e98e53f590ddac33366ecb5d2ab964d6d8b
Hussnain Ahmad <mrhussnainahmad@gmail.com>
2025-06-20 18:32:52 +0500

    fix navbars pt

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -236,7 +236,7 @@ const Navbar = () => {
               </button>
             </div>
 
-            <div className=" pt-[8px] pr-[6px] pb-[6px] pl-[6px] border-b border-base-300">
+            <div className=" pt-[3px] pr-[1px] pb-[1px] pl-[1px] border-b border-base-300">
               <div className="relative">
                 <input
                   type="text"
