5afd2b29f9a5fa430562353bfbbb61002cbf0179
Hussnain Ahmad <mrhussnainahmad@gmail.com>
2025-06-20 19:04:54 +0500

    fixing button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -90,7 +90,7 @@ const Sidebar = () => {
           
           <button
             onClick={() => setShowAddFriend(true)}
-            className="btn btn-sm btn-ghost hidden lg:flex"
+            className="btn btn-sm btn-ghost hidden md:flex"
             title="Add Friend"
           >
             <UserPlus className="size-4" />
@@ -161,7 +161,7 @@ const Sidebar = () => {
             <div className="mb-2">
               {showOnlineOnly ? "No online friends" : "No friends yet"}
             </div>
-            <div className="hidden lg:block">
+            <div className="hidden md:block">
               <button
                 onClick={() => setShowAddFriend(true)}
                 className="btn btn-sm btn-primary flex mx-auto"
