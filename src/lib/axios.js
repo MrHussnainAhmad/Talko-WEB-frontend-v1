3b354cd8bfdb3b09b6af65565126e2aef4d098c7
Hussnain Ahmad <mrhussnainahmad@gmail.com>
2025-07-12 15:19:47 +0500

    updating links

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://talkora-private-chat.up.railway.app/api",
+  baseURL: `${import.meta.env.VITE_BACKEND_URL || import.meta.env.VITE_BACKEND_LOCAL}/api`,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
